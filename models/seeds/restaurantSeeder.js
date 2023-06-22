2e6d5be53133904b246df254a2cb709d7177bbdc
taiwania <taiwaniajusto@gmail.com>
2023-06-22 21:39:28 +0800

    Add Restaurant Seeders

---
+++ models/seeds/restaurantSeeder.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const Restaurant = require("../restaurant");
+const restaurantList = require("../../restaurant.json").results;
+
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on("error", () => {
+  console.log("MongoDB is not connected.");
+});
+
+db.once("open", () => {
+  console.log("MongoDB is connected!");
+  Restaurant.create(restaurantList);
+  console.log("The data of restaurants is imported.");
+});
