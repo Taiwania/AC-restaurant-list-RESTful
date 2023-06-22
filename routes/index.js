92637941ff425a4736f402427f9061c3141a4eba
taiwania <taiwaniajusto@gmail.com>
2023-06-23 05:39:38 +0800

    Reconstruct the edit routers

--- routes/index.js
+++ routes/index.js
@@ -6,11 +6,13 @@ const router = express.Router()
 const home = require('./modules/home')
 const details = require('./modules/details')
 const addition = require('./modules/addition')
+const edit = require('./modules/edit')
 
 // set routes
 router.use('/', home)
 router.use('/restaurant', details)
 router.use('/', addition)
+router.use('/restaurant', edit)
 
-// Export routers
+// Export routes
 module.exports = router
---
+++ routes/modules/edit.js
@@ -0,0 +1,38 @@
+// Set Express and router
+const express = require("express");
+const router = express.Router();
+
+// Import restaurant model
+const Restaurant = require("../../models/restaurant");
+
+// Show the edit page
+router.get("/:id/edit", (req, res) => {
+  const id = req.params.id;
+  return Restaurant.findById(id)
+    .lean()
+    .then((restaurant) => res.render("edit", { restaurant }))
+    .catch((error) => console.log(error));
+});
+
+// Edit
+router.put("/:id", (req, res) => {
+  const id = req.params.id;
+  const editedRestaurant = {
+    name: req.body.name,
+    name_en: req.body.name_en,
+    category: req.body.category,
+    location: req.body.location,
+    google_map: req.body.google_map,
+    image: req.body.image,
+    phone: req.body.phone,
+    rating: req.body.rating,
+    description: req.body.description,
+  };
+
+  return Restaurant.findByIdAndUpdate(id, editedRestaurant)
+    .then(() => res.redirect(`/restaurant/${id}`))
+    .catch((error) => console.log(error));
+});
+
+// Export router
+module.exports = router;
