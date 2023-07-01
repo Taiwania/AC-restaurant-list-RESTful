c13f08d07f7bf4cc50cd6306f54ac5de8c00cc1d
taiwania <taiwaniajusto@gmail.com>
2023-07-01 16:44:59 +0800

    Set Express-session

--- app.js
+++ app.js
@@ -1,25 +1,37 @@
-// set mongoose, method-override, express, router and port
+// Set Express and port
 const express = require('express')
 const app = express()
 const port = 3000
+
+// Set related patches
 const methodOverride = require('method-override')
+const session = require('express-session')
+
+// Import the router
 const router = require('./routes')
 
-// mongoDB
+// Import Mongoose and MongoDB
 require('./config/mongoose')
 
-// set handlebars
+// Set and import Handlebars engine
 const exphbs = require('express-handlebars')
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
-// import bootstrap, popper, method-override, routers and URL encoder
+// Import related patches
+app.use(methodOverride('_method'))
+app.use(session({
+  secret: 'ACRestaurant',
+  resave: false,
+  saveUninitialized: true
+}))
+
+// Import Bootstrap, Popper and routers
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
-app.use(methodOverride('_method'))
 app.use(router)
 
-// online listener
+// Online listener
 app.listen(port, () => {
   console.log(`The website http://localhost:${port} is online.`)
 })
