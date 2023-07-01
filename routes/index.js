c91d4f297ab3a54ba081a0c23a53f8ec430bfbec
taiwania <taiwaniajusto@gmail.com>
2023-07-01 17:22:42 +0800

    Set middleware to auth the login statue

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,9 @@
 const express = require('express')
 const router = express.Router()
 
+// Define and import middleware
+const { authenticator } = require('../middleware/auth')
+
 // Import routes
 const home = require('./modules/home')
 const restaurant = require('./modules/restaurant')
@@ -9,10 +12,10 @@ const search = require('./modules/search')
 const users = require('./modules/users')
 
 // Set routes
-router.use('/', home)
-router.use('/restaurant', restaurant)
+router.use('/restaurant', authenticator, restaurant)
 router.use('/search', search)
 router.use('/users', users)
+router.use('/', authenticator, home)
 
 // Export routes
 module.exports = router
