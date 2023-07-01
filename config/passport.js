2027b1d36ce561dd06fa8abe154ab409e42a856f
taiwania <taiwaniajusto@gmail.com>
2023-07-02 01:31:55 +0800

    Add set brcypt function

--- config/passport.js
+++ config/passport.js
@@ -1,26 +1,35 @@
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
 const User = require('../models/user')
+const bcrypt = require('bcryptjs')
 
 module.exports = (app) => {
-  // Initialize and Import session 
+  // Initialize and Import session
   app.use(passport.initialize())
   app.use(passport.session())
-   
+
   // Main local login strategy
-  passport.use(new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
-    User.findOne({ email })
-      .then(user => {
-        if (!user) {
-          return done(null, false, { message: '該電郵帳號未註冊。That email is not registered!' })
-        }
-        if (user.password !== password) {
-          return done(null, false, { message: '電郵帳號或密碼不正確。Email or Password incorrect.' })
-        }
-        return done(null, user)
-      })
-      .catch(err => done(err, false))
-  }))
+  passport.use(
+    new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
+      User.findOne({ email })
+        .then((user) => {
+          if (!user) {
+            return done(null, false, {
+              message: '該電郵帳號未註冊。That email is not registered!'
+            })
+          }
+          return bcrypt.compare(password, user.password).then((isMatch) => {
+            if (!isMatch) {
+              return done(null, false, {
+                message: '電郵帳號或密碼不正確。Email or Password incorrect.'
+              })
+            }
+            return done(null, user)
+          })
+        })
+        .catch((err) => done(err, false))
+    })
+  )
 
   // Set serialize and deserialize
   passport.serializeUser((user, done) => {
@@ -29,7 +38,7 @@ module.exports = (app) => {
   passport.deserializeUser((id, done) => {
     User.findById(id)
       .lean()
-      .then(user => done(null, user))
-      .catch(err => done(err, null))
+      .then((user) => done(null, user))
+      .catch((err) => done(err, null))
   })
 }
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -3,6 +3,7 @@ const express = require('express')
 const router = express.Router()
 const User = require('../../models/user')
 const passport = require('passport')
+const bcrypt = require('bcryptjs')
 
 // Show the login page
 router.get('/login', (req, res) => {
@@ -61,15 +62,19 @@ router.post('/register', (req, res) => {
           password,
           confirmPassword
         })
-      } else {
-        return User.create({
-          name,
-          email,
-          password
-        })
-          .then(() => res.redirect('/'))
-          .catch((err) => console.log(err))
       }
+      return bcrypt
+        .genSalt(10)
+        .then((salt) => bcrypt.hash(password, salt))
+        .then((hash) =>
+          User.create({
+            name,
+            email,
+            password: hash
+          })
+        )
+        .then(() => res.redirect('/'))
+        .catch((err) => console.log(err))
     })
     .catch((err) => console.log(err))
 })
